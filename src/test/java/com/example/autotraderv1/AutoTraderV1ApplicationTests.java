eab77125dc3366557761279be13ddbee617e6563
SeungHyeonTak-sub <conficker77@gmail.com>
2022-10-26 00:55:25 +0900

    add, 프로젝트 생성

---
+++ src/test/java/com/example/autotraderv1/AutoTraderV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autotraderv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTraderV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
